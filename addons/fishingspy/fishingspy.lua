dc30032763d0e3f2c2542f251972bcb1508140f0
LunarRabbit <librarianrabbit@users.noreply.github.com>
2017-08-04 11:48:00 -0300

    Update fishingspy.lua

--- addons/fishingspy/fishingspy.lua
+++ addons/fishingspy/fishingspy.lua
@@ -14,12 +14,12 @@ end
 function FISHINGSPY_ON_FISHING_ITEM_LIST(frame, msg, argStr, argNum)
 	_G["_FISHINGSPY_ON_FISHING_ITEM_LIST"](frame, msg, argStr, argNum);
 
-    if argStr == nil or argStr == 'None' then
+	if argStr == nil or argStr == 'None' then
 		local header = GET_CHILD_RECURSIVELY(frame, "headerText");
 		header:SetText("{@st43}Tackle Box{/}");
 
-        return;
-    end
+		return;
+	end
 
 	local PcAID = argStr;
 	local list, cnt = SelectObject(GetMyPCObject(), 300, 'ALL');
